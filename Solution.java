8a360e315dbcd980d2fe32f3ae9e96ec0be43421
tenzinkoll56 <57824704+tenzinkoll56@users.noreply.github.com>
2020-12-18 12:13:42 +0600

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,166 +1,311 @@
 import java.util.*;
 
-public class Solution<Key extends Comparable<Key>, Value> {
-    private Node root; 
-    int size = 0;           
-    private class Node {
-        private Key key; 
-        private Value value;          
-        private Node left, right;  
-      
-        public Node(Key key, Value value) {
+public class BinarySearchTree<Key extends Comparable<Key>, Value> 
+{
+    private Node root;      // root of BST
+    int size = 0;          // number of nodes in subtree
+
+    private class Node 
+    {
+        private Key key;           // sorted by key
+        private Value val;         // associated data
+        private Node left, right;  // left and right subtrees
+       
+        public Node(Key key, Value val) //Node constructor
+        {
             this.key = key;
-            this.value = value;
-            
+            this.val = val;
         }
     }
 
-   
-    public Solution() {
-    	root = null;
-    }
-
-    
-    public boolean isEmpty() {
-    	if(size() == 0){
-    		return true;
-    	}
-    	else{
-    		return false;
-    	}
-    }
-
-    
-    public int size() {
-    	return size;
-    }
-
-   
-    
-    
-    public Value get(Key key) {
- 		if(key == null){
-			throw new IllegalArgumentException("Error");
-		}
- 		return get(root, key);
-    }
-    private Value get(Node x, Key key) {
- 		if(x == null){
- 			return null;
- 		}
-			int cmp = key.compareTo(x.key);
-			if (cmp < 0){
-				return get(x.left, key);
-			} 
-			else if (cmp > 0){
-				return get(x.right, key);
-			}			
-			else{
-				return x.val;
-			}
-		}
-   
-    public void put(Key key, Value val) {
-    	if (key == null){
-    		throw new IllegalAccessException ("Error");
-    	}
-    	root = put(root, key, val);    
-    }
-
-    private Node put(Node x, Key key, Value val) {
-    	if(x == null){
-    	 	return new Node(key, val, 0);
-    	}
-	    	int cmp = key.compareTo(x.key);
-	    	if(cmp < 0){
-	    		x.left = put(x.left, key, val);
-	    	}
-	    	else if(cmp > 0){
-	    		x.right = put(x.right, key, val);
-	    	}
-	    	else{
-	    		x.val = val;
-	    	}
-	    	x.size = 1 + size(x.left) + size(x.right);
-	    	return x;
-	  }
-
-    
-    public Key min() {
-    	return min(root).key;
-    } 
-
-    private Node min(Node x) { 
-        if (x.left == null) {
-        	return x;
+    // returns empty symbol table
+    public boolean isEmpty() 
+    {
+        if(size() == 0)	//if the size is zero
+        {
+            return true;
         }
-        return min (x.left); 
-    } 
+        return false;       
+    }
 
-   
+   //returns number of key-value pairs
+    public int size() 
+    {
+        return size(root);       //Check size at node root
+    }
 
+    // return number of key-value pairs in BST rooted at x
+    private int size(Node x) 
+    {
+        if(x == null)	//if the symbol table is empty
+            return 0;
+        else
+            return size;
+    }
     
-    public Key floor(Key key) {
-        Node x = floor(root, key);
-        if(x == null){
-        	return null;
+    public boolean contains(Key key) 
+    {
+    	//should not accept null key
+		if (key == null) 
+        {
+            throw new IllegalArgumentException("Key cannot be null"); 
         }
-        return x.key;
+        return get(key) != null;//when key is not null 
+    }
+
+    //returns value associated with key
+    public Value get(Key key) 
+    {
+        if(key == null)	//should not accept null key
+        {
+	        throw new IllegalArgumentException("Argument passed in key is null");
+	    }
+        Node current = root;	// temporary external variable for traversing
+        while(key != current.key)
+        {
+        	int compare = key.compareTo(current.key); //compare the key with each key of a node
+        	if(compare < 0)
+            {
+        		current = current.left; //if key is less than root key
+        	}
+	        else if(compare > 0)	//else
+            {
+	        	current = current.right;
+	        }
+        }
+        return current.val;
+    }
+    
+    public void put(Key key, Value val) 
+    {
+        if (key == null) 	//if key is null
+        {
+            throw new IllegalArgumentException("Key cannot be null");
+        }  
+
+        Node newest = new Node(key, val); // create new node
+        if(root == null)
+        {
+            root = newest;	//when the root is null
+        }
+        else
+        {
+        	Node current = root;	//for traversing through the bst 
+        	Node parent;
+
+        	while(true)
+        	{
+        		parent = current;	// parent node is current
+        		int comp = key.compareTo(current.key);//compare the key with each key in node
+	            if(comp < 0)	// to put on the left
+	            {
+	                current = current.left;
+                    if(current == null)
+                    {
+                        parent.left = newest;
+                        size += 1;
+                        return;
+                    }
+	            }
+	            else if(comp > 0)	// put in the right
+	            {
+	            	current = current.right;
+	            	if(current == null)
+	            	{
+	            		parent.right = newest;
+                        size += 1;
+                        return;
+	            	}
+	            }
+	            else
+	            {
+	            	current.val = val;
+	                return;
+        		}    
+	        }            
+        }
+        size += 1;
+    }
+
+    public Key min() 
+    {
+		if (isEmpty()) //For empty symbol table
+        {
+            throw new NoSuchElementException("Empty symbol table");
+        }
+
+        Node current = root;
+
+        while (current.left != null) //Finding left most node 
+        { 
+            current = current.left; 
+        }
+        return current.key;         
     } 
 
-    private Node floor(Node x, Key key) {
-    	if(x == null){
-    		return null;
-    	}
-    	int cmp = key.compareTo(x.key);
-    	if(cmp ==0){
-    		return x;
-    	}
-    	if(cmp < 0){
-    		return floor(x.left,key);
-    	}
-    	Node t = floor(x.right, key);
-    	if(t != null){
-    		return t;
-    	}
-    	else{
-    		return x;
-    	}
+    // delete the minimum key in symbol table
+    public void deleteMin()
+    {
+        if (isEmpty()) //For empty symbol table
+        {
+            throw new NoSuchElementException("Empty symbol table");
+        }
+
+        Node current = root;
+
+        while (current.left != null) //Finding left most node 
+        { 
+            current = current.left; 
+        }
+        current = null;
+        size--; 
+    }
+
+    public void delete(Key key)
+    {
+        if (isEmpty()) //For empty symbol table
+        {
+            throw new NoSuchElementException("Empty symbol table");
+        }
+
+        Node current = root;    
+        while(key != current.key)
+        {
+            int compare = key.compareTo(current.key); 
+            if(compare < 0)
+            {
+                current = current.left; //if key is less than root key
+            }
+            else if(compare > 0)    //else
+            {
+                current = current.right;
+            }
+        }
+        current.key = null;
+        size--;
+    }
+    //returns the largest key less than or equal to key
+    public Key floor(Key key) 
+    {
+        if(key == null)	//cannot have null key argument
+            throw new IllegalArgumentException("Argument passed in key is null");
+
+        if(isEmpty())
+        {
+            throw new NoSuchElementException("Empty symbol table");
+        }
+        Node current = floor(root, key);	// To traverse the bst starting from root
+        if(current == null)
+        {
+            return null;
+        }
+        else 
+        {
+            return current.key;
+        }
+
     } 
 
+    private Node floor(Node x, Key key) 
+    {
+        Node floor = null;	
+        while(x != null)
+        {
+            int compare = key.compareTo(x.key);
+            if(compare == 0)
+            {
+                return x;
+            }
+            else if(compare < 0)
+            {
+                x = x.left;
+            }
+            else
+            {
+                floor = x;
+                x = x.right;
+            }
+        }
+        return floor;
+       
+    } 
 
-    
-    
+    // return the key whose rank is k
+    public Key select(int k) 
+    {
+        if(k == 0)	//Null rank passed
+        {
+            throw new IllegalArgumentException("argument to get value() is null");
+        }
+        Node current = root; // current node is root
+        while(k != 0)//when k is not null
+        {
+            int left = size(current.left);	//Left size of left 
+            if(left > k)
+            {
+                current = current.left;
+            }
+            else if(left < k)
+            {
+                current = current.right;
+                k = k - left - 1;
+            }
+            //else
+              //  return current.key;
+        }
+        return current.key;
+    }
 
-    
-    // public Key select(int k) {
-        
-    // }
+    // returns all the keys in symbol table between the given range as an iterables
+    public Iterable<Key> keys(Key lo, Key hi) 
+    {
+    	if(lo == null || hi == null)
+    		throw new IllegalArgumentException("Minimum and Maximum endpoint should not be null");
 
-   
+        Stack<Node> stack = new Stack<Node>();
+        ArrayList<Key> keys = new ArrayList<Key>();
+        Node x = root;
+        while (x != null || !stack.isEmpty()) 
+        {
+            if (x != null) 
+            {
+                stack.push(x);
+                x = x.left;
+            }
+            else 
+            {
+                x = stack.pop();
+                keys.add(x.key);
+                x = x.right;
+            }
+        }
+        return keys;
+    } 
+  
+    public static void main(String[] args) 
+    { 
+        BinarySearchTree<String, Integer> obj = new BinarySearchTree<String, Integer>();
 
-   
-    public static void main(String[] args) { 
-        Solution <String, Integer> obj = new Solution<String, Integer>();
-        
-
-        obj.put("Ada",1);
-        obj.put("Ballerian",3);        
+        obj.put("Ada", 1);
+        obj.put("Ballerina", 3);
         System.out.println(obj.get("Ada"));
-        obj.put("Html",5);
+        obj.put("Html", 5);
+        obj.put("Java", 7);
         System.out.println(obj.get("Java"));
         System.out.println(obj.size());
         System.out.println(obj.min());
-        System.out.println(obj.floor("Ballerian"));
-        obj.put("Java",8);
-        obj.put("Dart",9);
+        System.out.println(obj.min());
+        System.out.println(obj.floor("Ballerina"));
+        //System.out.println(obj.select(3));
+        System.out.println(obj.keys("Ada", "Java"));
+        obj.put("Java", 8);
+        obj.put("Dart", 9);
         System.out.println(obj.get("Java"));
         System.out.println(obj.size());
-
-
-
-
-
-       
+        obj.deleteMin();
+        System.out.println(obj.keys("Ballerina", "Java"));
+        obj.delete("Java");
     }
 }
+
+
